10c0c4bc24bf847915dcbaa371433fd634e011e2
佳伟 <s2265681@163.com>
2024-06-04 14:52:24 +0800

    feat: first commit

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        WebView(url: URL(string: "https://www.baidu.com")!)
     }
 }
 
